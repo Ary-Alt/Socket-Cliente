087e09fce4072ac46713d3aeb6809e4ef8092117
Ari <michelortiz726@gmail.com>
2022-07-13 14:51:57 -0500

    Implementacion de promesa y cambio de pantalla despues de logeo

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,34 +1,47 @@
 import { Injectable } from '@angular/core';
-import { Socket} from 'ngx-socket-io';
-
+import { Socket } from 'ngx-socket-io';
+import { Usuario } from '../Classes/Usuario';
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
-  public socketStatus=false;
-
+  public socketStatus = false;
+  public usuario?: Usuario;
   constructor(
-    private socket: Socket
-  ) {
-    this.checkStatus();
-  }
+    private socket: Socket,
+    ) {
+      this.checkStatus();
+    }
 
-  checkStatus(){
-    this.socket.on('connect', () => {
-      console.log('conectado al servidor');
-      this.socketStatus = true;
-    })
-   
-    this.socket.on('disconnect', () => {
-      console.log('Desconectado del servidor');
-      this.socketStatus = false;
-    })
-  }
-  emit(evento: string, payload?:any, callback?: Function){
-    this.socket.emit( evento, payload, callback );
-    
-  }
-  listen( evento: string){
-    return this.socket.fromEvent( evento );
-  }
+    checkStatus(){
+      this.socket.on('connect', () => {
+        console.log("Conectado al servidor");
+        this.socketStatus = true;
+      });
+
+      this.socket.on('disconnect', () => {
+        console.log("Desconectado del servidor");
+        this.socketStatus = true;
+      });
+    }
+
+    emit(evento: string, payload?:any, callback?:Function){
+            
+      this.socket.emit( evento, payload, callback );
+    }
+    listen(evento: string){
+      return this.socket.fromEvent( evento );
+    }
+  
+    loginWS( nombre: string)
+    {
+     
+      return new Promise<void>( ( resolve, reject) => {
+        this.emit('configurar-usuario', { nombre }, (resp:Response) => 
+      {
+          resolve();
+      });
+      });
+      
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   wsUrl: 'http://localhost:5000'
-  /*wsUrl: 'http://10.10.60.12:5000'*/
+  // wsUrl: 'http://10.10.60.27:5000'
+  // wsUrl: 'http://10.10.60.14:5000'
   
 
 };
